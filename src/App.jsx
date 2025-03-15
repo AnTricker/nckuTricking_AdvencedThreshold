c506a22670a9fd4a16d165fb6184f878c0ca37fa
AnTricker <anpublic181@gmail.com>
2025-03-15 18:41:31 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
             <span> Advenced </span>
           </div>
           <div className={styles.logo}>
-            <img src="{`${process.env.PUBLIC_URL}/Tricking_logo.png`}" alt="[ncku Tricking]" />
+            <img src="{`${process.env.PUBLIC_URL}/Tricking_logo.png`}" alt="ncku Tricking" />
           </div>
           <div className={styles.titleRight}>
             <span> Threshold </span>

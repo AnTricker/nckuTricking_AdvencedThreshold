1aab9ec0aacf330f83073e619f59d29c852ef49a
An <anpublic181@gmail.com>
2025-03-22 23:32:55 +0800

    upload our videos

--- src/components/TrickDetail.jsx
+++ src/components/TrickDetail.jsx
@@ -9,30 +9,31 @@ export default function TrickDetail({ trickData, trickId }) {
     setTrick(foundTrick);
   }, [trickId]);
 
-  return <div 
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      width: "50%",
-      marginLeft: "auto",
-      marginRight: "auto",
-      padding: "10px",
-      color: "white",
-    }}
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "50%",
+        marginLeft: "auto",
+        marginRight: "auto",
+        padding: "10px",
+        color: "white",
+      }}
     >
       <div>
-        <h1 style={{ color:"red", textAlign: "center" }}> {trick.name} </h1>
+        <h1 style={{ color: "red", textAlign: "center" }}> {trick.name} </h1>
       </div>
       <div>
         <video
           style={{
-            margin: "5px",
+            margin: "10px",
             width: "100%",
             height: "auto",
           }}
-          src= {`${import.meta.env.BASE_URL}assets/${trick.id}.mp4`}
+          src={`${import.meta.env.BASE_URL}assets/${trick.id}.mp4`}
           autoPlay
           loop
           muted
@@ -41,12 +42,21 @@ export default function TrickDetail({ trickData, trickId }) {
         />
       </div>
 
-      <div style={{margin:"10px", padding:"10px", color:"white", backgroundColor:"red", borderRadius:"10px"}}>
+      <div
+        style={{
+          margin: "10px",
+          padding: "10px",
+          color: "white",
+          backgroundColor: "red",
+          borderRadius: "10px",
+        }}
+      >
         <strong> {trick.type} </strong>
       </div>
 
-      <div style={{padding:"10px", color:"white"}}>
-      <p> {trick.introduction} </p>
+      <div style={{ padding: "10px", color: "white" }}>
+        <p> {trick.introduction} </p>
       </div>
-  </div>
+    </div>
+  );
 }
